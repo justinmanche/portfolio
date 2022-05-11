6606e68502e17ce4931805a6ce0936720df441f9
Justin Manche <jmanche@australiacloud.com.au>
2022-05-11 22:23:55 +1000

    Testing a super basic site

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = ({ children }) => (
+  <>
+    <header>
+    </header>
+    <main>
+      {children}
+    </main>
+    <footer>
+
+    </footer>
+  </>
+)
+
+export default Layout
