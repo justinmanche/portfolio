a23089650bae8832ac6969c1257604aacdee2be7
Justin Manche <jmanche@australiacloud.com.au>
2022-05-12 23:27:37 +1000

    Add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: `portfolio`,
     siteUrl: `https://www.justinmanche.com`,
   },
-  plugins: [`gatsby-plugin-netlify`]
+  plugins: [
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/logo.svg',
+      },
+    },
+  ]
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import './layout.css'
 
 const Layout = ({ children }) => (
   <>
     <header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>hi all</title>
+        <link rel="canonical" href="http://justimanche.com" />
+      </Helmet>
     </header>
     <main>
       <h1>justinmanche.com</h1>
